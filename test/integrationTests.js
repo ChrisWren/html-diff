dff0230a5bd9c817f9557e427e6b55abba984784
ChrisWren <cthewren@gmail.com>
2013-09-14 13:37:18 -0700

    Added tests checking return value

--- test/integrationTests.js
+++ test/integrationTests.js
@@ -9,43 +9,85 @@ describe('html-diff', function () {
 
   describe('when run on folders with html files to diff', function() {
 
-    it('should log files that are unique to each folder', function() {
-      var consoleSpy = sinon.stub(console, 'log');
-      htmlDiff([{
-        path: 'test/fixtures/templateEngine1',
-        name: 'engine1'
-      }, {
-        path: 'test/fixtures/templateEngine2',
-        name: 'engine2'
-      }]);
-      consoleSpy.firstCall.args[0].should.include('which was not found for');
-      console.log.restore();
-    });
+    describe('if the folders have different contents', function () {
 
-    it('should log the differences between files', function() {
-      var stdoutSpy = sinon.stub(process.stdout, 'write');
-      htmlDiff([{
-        path: 'test/fixtures/templateEngine1',
-        name: 'engine1'
-      }, {
-        path: 'test/fixtures/templateEngine2',
-        name: 'engine2'
-      }]);
-      stdoutSpy.lastCall.args[0].should.include('Content');
-      process.stdout.write.restore();
-    });
+      it('should log files that are unique to each folder', function() {
+        var consoleSpy = sinon.stub(console, 'log');
+        htmlDiff([{
+          path: 'test/fixtures/templateEngine1',
+          name: 'engine1'
+        }, {
+          path: 'test/fixtures/templateEngine2',
+          name: 'engine2'
+        }]);
+        consoleSpy.firstCall.args[0].should.include('which was not found for');
+        console.log.restore();
+      });
 
-    it('should log a success message if the folders have the same contents', function() {
-      var consoleSpy = sinon.stub(console, 'log');
-      htmlDiff([{
-        path: 'test/fixtures/templateEngine1',
-        name: 'engine1'
-      }, {
-        path: 'test/fixtures/templateEngine1Clone',
-        name: 'engine1Clone'
-      }]);
-      consoleSpy.lastCall.args[0].should.include('All files matching');
-      console.log.restore();
+      it('should log the differences between files', function() {
+        var stdoutSpy = sinon.stub(process.stdout, 'write');
+        htmlDiff([{
+          path: 'test/fixtures/templateEngine1',
+          name: 'engine1'
+        }, {
+          path: 'test/fixtures/templateEngine2',
+          name: 'engine2'
+        }]);
+        stdoutSpy.lastCall.args[0].should.include('Content');
+        process.stdout.write.restore();
+      });
+
+      it('should log the differences between files', function() {
+        var stdoutSpy = sinon.stub(process.stdout, 'write');
+        htmlDiff([{
+          path: 'test/fixtures/templateEngine1',
+          name: 'engine1'
+        }, {
+          path: 'test/fixtures/templateEngine2',
+          name: 'engine2'
+        }]);
+        stdoutSpy.lastCall.args[0].should.include('Content');
+        process.stdout.write.restore();
+      });
+
+      it('should return false', function() {
+        sinon.stub(process.stdout, 'write');
+        htmlDiff([{
+          path: 'test/fixtures/templateEngine1',
+          name: 'engine1'
+        }, {
+          path: 'test/fixtures/templateEngine2',
+          name: 'engine2'
+        }]).should.eql(false);
+        process.stdout.write.restore();
+      });
+    });
+    describe('if the folders have the same contents', function () {
+
+      it('should log a success message', function() {
+        var consoleSpy = sinon.stub(console, 'log');
+        htmlDiff([{
+          path: 'test/fixtures/templateEngine1',
+          name: 'engine1'
+        }, {
+          path: 'test/fixtures/templateEngine1Clone',
+          name: 'engine1Clone'
+        }]);
+        consoleSpy.lastCall.args[0].should.include('All files matching');
+        console.log.restore();
+      });
+
+      it('should return true', function() {
+        sinon.stub(console, 'log');
+        htmlDiff([{
+          path: 'test/fixtures/templateEngine1',
+          name: 'engine1'
+        }, {
+          path: 'test/fixtures/templateEngine1Clone',
+          name: 'engine1Clone'
+        }]).should.eql(true);
+        console.log.restore();
+      });
     });
   });
 });
